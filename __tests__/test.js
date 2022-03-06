6a973371d91353c75d4a2a90d776cc62a096bc81
Ilya Solonovich <ilyasolonovich20@gmail.com>
2022-03-06 16:22:29 +0300

    update

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,25 +1,39 @@
-#!/usr/bin/env node
 import fs from 'fs';
-import path from 'path';
-import gendiff from '../index.js';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { test, expect } from '@jest/globals';
+import _ from 'lodash';
+import genDiff from '../index.js';
 
-const interExtensionNames = ['json', 'yml'];
 
-const getFilePath = (extensionNames) => extensionNames.map((extensionName) => ([
-  path.resolve( `__fixtures__/Before.${extensionName}`),
-  path.resolve( `__fixtures__/After.${extensionName}`),
-]));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const getResultPath = (format) => {
-  const result = path.resolve(__dirname, `__fixtures__/diff-${format}.txt`);
-  return fs.readFileSync(result, 'utf8');
+const getFixturePath = (filename, ext) => path
+  .join(__dirname, '..', '__fixtures__', `${filename}.${ext}`);
+
+const getExpectedResult = (outputFormatterType) => {
+  const expectedResultFileName = `diff-${(outputFormatterType)}`;
+  const expectedResultFilePath = getFixturePath(expectedResultFileName, 'txt');
+
+  return fs.readFileSync(expectedResultFilePath, 'utf-8');
 };
 
-test.each(getFilePath(interExtensionNames))(
-  'generate difference between two configuration files',
-  (beforeFilePath, afterFilePath) => {
-    expect(gendiff(beforeFilePath, afterFilePath, 'pretty')).toEqual(getResultPath('pretty'));
-    expect(gendiff(beforeFilePath, afterFilePath, 'plain')).toEqual(getResultPath('plain'));
-    expect(gendiff(beforeFilePath, afterFilePath, 'json')).toEqual(getResultPath('json'));
+const inputDataTypes = ['json', 'yml'];
+const outputFormatterTypes = ['stylish', 'plain', 'json'];
+const getTestsSet = (types1, types2) => types1
+  .flatMap((type1) => types2
+    .map((type2) => ([type1, type2])));
+
+test.each(getTestsSet(inputDataTypes, outputFormatterTypes))(
+  'genDiff read %s files and generate %s format output',
+  (inputDataType, outputFormatterType) => {
+    const filepathBefore = getFixturePath('Before', inputDataType);
+    const filepathAfter = getFixturePath('After', inputDataType);
+    const currentResult = genDiff(filepathBefore, filepathAfter, outputFormatterType);
+
+    const expectedResult = getExpectedResult(outputFormatterType);
+
+    expect(currentResult).toEqual(expectedResult);
   },
 );
