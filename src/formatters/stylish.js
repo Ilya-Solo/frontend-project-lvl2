ee2a2acd1860ed85470242d58947fc477b1ae9b4
Ilya Solonovich <ilyasolonovich20@gmail.com>
2022-03-09 18:29:55 +0300

    change depth counter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,18 +1,16 @@
 import _ from 'lodash';
 
 const tab = ' ';
-const addTab = 4;
-const decreaseTab = 1;
 
 const render = (value, depth, stringify) => {
   if (!_.isObject(value)) {
     return value;
   }
-  const tabCount = depth + addTab;
-  const convertObjectToString = ([key, objectValue]) => `${stringify(key, render(objectValue, tabCount, stringify), tabCount, ' ')}`;
-  return `{\n${(Object.entries(value).map(convertObjectToString)).join('\n')}\n${tab.repeat(tabCount - decreaseTab)} }`;
+  const newDepth = depth + 1;
+  const convertObjectToString = ([key, objectValue]) => `${stringify(key, render(objectValue, newDepth, stringify), newDepth, ' ')}`;
+  return `{\n${(Object.entries(value).map(convertObjectToString)).join('\n')}\n${tab.repeat((newDepth * 4))}}`;
 };
-const stringify = (key, value, depth, sign) => `${tab.repeat(depth)}  ${sign} ${key}: ${render(value, depth, stringify)}`;
+const stringify = (key, value, depth, sign) => `${tab.repeat(depth * 4)}  ${sign} ${key}: ${render(value, depth, stringify)}`;
 
 const mapping = {
 
@@ -25,7 +23,7 @@ const mapping = {
   ],
   nested: (obj, depth, innerFormat) => stringify(
     obj.key,
-    innerFormat(obj.value1, depth + addTab),
+    innerFormat(obj.value1, depth + 1),
     depth,
     ' ',
   ),
@@ -34,7 +32,7 @@ const format = (diffTree) => {
   const innerFormat = (innerDiffTree, depth) => {
     const strings = innerDiffTree
       .flatMap((obj) => mapping[obj.type](obj, depth, innerFormat));
-    const framedStrigs = ['{', ...strings, `${tab.repeat(depth)}}`];
+    const framedStrigs = ['{', ...strings, `${tab.repeat(depth * 4)}}`];
     return framedStrigs.join('\n');
   };
 
